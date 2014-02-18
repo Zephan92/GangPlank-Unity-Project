affe890d406cee9b68e602dce0a0f0444a085f4a
Renar Narubin <rnarubin@hawk.iit.edu>
2014-02-18 15:47:17 -0600

    added key value pair traversal

--- server/hashtable.c
+++ server/hashtable.c
@@ -1,79 +1,88 @@
 #include "hashtable.h"
 
-static uint32_t hash_code(const char* key);
-static node_t *ht_get_node(hashtable_t*,const char*);
-static node_t *ht_get_node_i(hashtable_t*,const char*,uint32_t);
+static node_t *ht_get_node(hashtable_t*,const void*);
+static node_t *ht_get_node_i(hashtable_t*,const void*,uint32_t);
 static void ht_rehash(hashtable_t*, size_t);
 
-void ht_init_s(hashtable_t *t, size_t capacity){
+void ht_init_void(hashtable_t *t, int (*cmp)(const void*, const void*), uint32_t (*hash_code)(const void*)){
+	ht_init_void_s(t, 16, cmp, hash_code);
+}
+
+void ht_init_void_s(hashtable_t *t, size_t capacity, int (*cmp)(const void*, const void*), uint32_t (*hash_code)(const void*)){
 	uint32_t cap = 1 << (31 - __builtin_clz(capacity));
 	if(capacity > cap) cap <<= 1;
 
 	t->cap = cap;
 	t->arr = (node_t**)calloc(t->cap, sizeof(node_t*));
 	t->size = 0;
+	t->cmp = cmp;
+	t->hash_code = hash_code;
+}
+
+void ht_init_s(hashtable_t *t, size_t capacity){
+	HT_INIT_VOID_S(t, capacity, string_eq, string_hash);
 }
 
 void ht_init(hashtable_t *t){
 	ht_init_s(t, 16);
 }
 
-void *ht_add(hashtable_t *t, const char* key, void *val){
+void *ht_add(hashtable_t *t, const void* key, void *val){
 	if(t->size >= (t->cap >> 1) + (t->cap >> 2)){
 		ht_rehash(t, t->cap << 1);
 	}
 
-	uint32_t index = hash_code(key) & (t->cap - 1);
+	uint32_t index = t->hash_code(key) & (t->cap - 1);
 	void *ret = NULL;
 	node_t *n = ht_get_node_i(t, key, index);
 	if(!n){
 		n = (node_t*)malloc(sizeof(node_t));
 		n->next = t->arr[index];
-		n->key = key;
+		n->kvp.key = key;
 
 		t->arr[index] = n;
 		t->size++;
 	}
 	else{
-		ret = n->val;
+		ret = n->kvp.val;
 	}
-	n->val = val;
+	n->kvp.val = val;
 
 	return ret;
 }
 
-void *ht_get(hashtable_t *t, const char* key){
+void *ht_get(hashtable_t *t, const void* key){
 	node_t *n = ht_get_node(t, key);
-	return n?n->val:NULL;
+	return n?n->kvp.val:NULL;
 }
 
-node_t *ht_get_node(hashtable_t *t, const char* key){
-	return ht_get_node_i(t, key, hash_code(key) & (t->cap - 1));
+node_t *ht_get_node(hashtable_t *t, const void* key){
+	return ht_get_node_i(t, key, t->hash_code(key) & (t->cap - 1));
 }
 
-node_t *ht_get_node_i(hashtable_t *t, const char* key, uint32_t index){
+node_t *ht_get_node_i(hashtable_t *t, const void* key, uint32_t index){
 	node_t *n;
-	for(n = t->arr[index]; n && strcmp(key, n->key); n = n->next);
+	for(n = t->arr[index]; n && !t->cmp(key, n->kvp.key); n = n->next);
 	return n;
 }
 
-void *ht_delete(hashtable_t *t, const char*key){
-	uint32_t index = hash_code(key) & (t->cap-1);
+void *ht_delete(hashtable_t *t, const void*key){
+	uint32_t index = t->hash_code(key) & (t->cap-1);
 	void *ret = NULL;
 	node_t *n = t->arr[index];
 	if(!n) return ret;
-	if(strcmp(key,n->key) == 0){
+	if(t->cmp(key,n->kvp.key)){
 		t->arr[index] = n->next;
-		ret = n->val;
+		ret = n->kvp.val;
 		free(n);
 		t->size--;
 	}
 	else{
-		for(; n->next && strcmp(key, n->next->key); n = n->next);
+		for(; n->next && !t->cmp(key, n->next->kvp.key); n = n->next);
 		if(n->next){
 			node_t *target = n->next;
 			n->next = target->next;
-			ret = target->val;
+			ret = target->kvp.val;
 			free(target);
 			t->size--;
 		}
@@ -84,11 +93,11 @@ void *ht_delete(hashtable_t *t, const char*key){
 	return ret;
 }
 
-void subrehash(node_t **newarr, size_t newcap, node_t *n){
+void subrehash(hashtable_t *t, node_t **newarr, size_t newcap, node_t *n){
 	if(n->next){
-		subrehash(newarr, newcap, n->next);
+		subrehash(t, newarr, newcap, n->next);
 	}
-	uint32_t index = hash_code(n->key) & (newcap-1);
+	uint32_t index = t->hash_code(n->kvp.key) & (newcap-1);
 	n->next = newarr[index];
 	newarr[index] = n;
 }
@@ -98,7 +107,7 @@ void ht_rehash(hashtable_t *t, size_t newcap){
 	uint32_t i;
 	for(i = 0; i < t->cap; i++){
 		if(t->arr[i]){
-			subrehash(newarr, newcap, t->arr[i]);		
+			subrehash(t, newarr, newcap, t->arr[i]);		
 		}
 	}
 	free(t->arr);
@@ -125,10 +134,10 @@ void ht_dispose(hashtable_t *t){
 	free(t->arr);
 }
 
-#define FNV_PRIME 16777619
-#define FNV_BASE 2166136261
+#define FNV_PRIME 0x1000193
+#define FNV_BASE 0x811C9DC5 
 
-uint32_t hash_code(const char* key){
+uint32_t string_hash(const char* key){
 	uint32_t i, hc = FNV_BASE;
 	for(i = 0; key[i]; i++){
 		hc ^= key[i];
@@ -137,7 +146,24 @@ uint32_t hash_code(const char* key){
 	return hc;
 }
 
+int string_eq(const char *a, const char *b){
+	return strcmp(a, b) == 0;
+}
+
+void get_all_kvps(hashtable_t *t, kvp_t *kvps){
+	node_t *n;
+	uint32_t i, j = 0;
+	for(i = 0; i < t->cap; i++){
+		n = t->arr[i];
+		while(n){
+			kvps[j++] = n->kvp;
+			n = n->next;
+		}
+	}
+}
+
 /*
+#include <stdio.h>
 void ht_print(hashtable_t *t){
 	node_t *n;
 	uint32_t i;
@@ -146,7 +172,7 @@ void ht_print(hashtable_t *t){
 		n = t->arr[i];
 		while(1){
 			if(n){
-				printf("k:'%s' v:%p -> ",n->key,n->val);
+				printf("k:%p v:%p -> ",n->kvp.key,n->kvp.val);
 				n = n->next;
 			}
 			else{
