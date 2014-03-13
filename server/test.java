99c1e82622b5305f5b09a15b356ea5d50a622f29
Renar Narubin <rnarubin@hawk.iit.edu>
2014-03-13 16:07:08 -0500

    server: adjusted test file to delimiters

--- server/test.java
+++ server/test.java
@@ -2,33 +2,38 @@ import java.util.*;
 import java.io.*;
 import java.net.*;
 
+import static java.lang.System.out;
+
 public class test{
+
+	static String ip_addr = "54.200.46.105";
 	public static void main(String args[]) throws Exception{
 		Scanner scan = new Scanner(System.in);
-		Socket socket = new Socket("127.0.0.1",args.length>0?Integer.parseInt(args[0]):5555);
+		//*
+		Socket socket = new Socket(args.length>1?args[1]:ip_addr,args.length>0?Integer.parseInt(args[0]):5555);
 		BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 		PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+//*/
 
-		out.print("add;foobar\0");
-		out.flush();
-		System.out.println(in.readLine());
+		// \x17 ^W end of message
+		// \x1F ^^ split cmd/msg
+		// \x1E ^_ split multi message before \x17
+		char[] buff = new char[1024];
+		while(scan.hasNext()){
+			String s = scan.nextLine().replace("\n","")+"\0";
+			out.print(s);
+			out.flush();
 
-		scan.nextLine();
-		out.print("get;foobar\0");
-		out.flush();
-		System.out.println(in.readLine());
-
-		scan.nextLine();
-		out.print("rm;foobar\0");
-		out.flush();
-		System.out.println(in.readLine());
-		/*
-		String read;
-		while((read = in.readLine()) != null){
-			System.out.println("in:"+read);
+			String b = "";
+			do{
+				int read = in.read(buff, 0, 1024);
+				String q = new String(buff, 0, read);
+				b += q;
+			}while(b.charAt(b.length()-1) != (char)0x17);
+			System.out.println("{"+b+"}");
 		}
-		*/
 
 		socket.close();
+//*/
 	}
 }
